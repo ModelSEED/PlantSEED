43633020a710cea29d699d2d9e1525479706778c
Sam Seaver <samseaver@gmail.com>
2020-12-11 13:32:06 -0600

    Resorting to avoid merging spontaneous reactions

--- Scripts/PlantSEED_v3/Curation/Sort_Roles.py
+++ Scripts/PlantSEED_v3/Curation/Sort_Roles.py
@@ -4,13 +4,9 @@ import json
 with open("../../../Data/PlantSEED_v3/PlantSEED_Roles.json") as subsystem_file:
     roles_list = json.load(subsystem_file)
 
-roles_dict=dict()
-for entry in roles_list:
-    roles_dict[entry['role']]=entry
-
 new_roles_list=list()
-for role in sorted(roles_dict.keys()):
-    new_roles_list.append(roles_dict[role])
+for role in sorted(roles_list, key = lambda entry:entry['role']):
+    new_roles_list.append(role)
 
 with open('../../../Data/PlantSEED_v3/PlantSEED_Roles.json','w') as new_subsystem_file:
     json.dump(new_roles_list,new_subsystem_file,indent=4)
