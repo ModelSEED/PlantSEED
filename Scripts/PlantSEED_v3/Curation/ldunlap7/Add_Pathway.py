9efd1e42644e31bdf852536d693e7886ce24d3ca
Ldunlap7 <89104047+Ldunlap7@users.noreply.github.com>
2021-09-24 10:40:32 -0400

    Revealed commented out code

--- Scripts/PlantSEED_v3/Curation/ldunlap7/Add_Pathway.py
+++ Scripts/PlantSEED_v3/Curation/ldunlap7/Add_Pathway.py
@@ -97,6 +97,6 @@ with open(pwy_file) as pwy_file_handle:
 
             roles_list.append(new_role)
 
-#with open('../../../../Data/PlantSEED_v3/PlantSEED_Roles.json','w') as new_subsystem_file:
-    #json.dump(roles_list,new_subsystem_file,indent=4)
+with open('../../../../Data/PlantSEED_v3/PlantSEED_Roles.json','w') as new_subsystem_file:
+    json.dump(roles_list,new_subsystem_file,indent=4)
 # print(json.dumps(roles_list,indent=4))
