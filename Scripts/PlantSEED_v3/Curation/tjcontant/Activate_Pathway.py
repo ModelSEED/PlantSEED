0cad6868a5d6f2f4122503f5cb7a8f8feb4e20a9
Tristan J. Contant <tjcontant@icloud.com>
2022-05-17 13:11:32 -0500

    Create Activate_Pathway.py
    a new script to change the 'include' parameter of list of roles to True (thus activating them in the model)

---
+++ Scripts/PlantSEED_v3/Curation/tjcontant/Activate_Pathway.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os,sys,json
+
+if(len(sys.argv)<2 or os.path.isfile(sys.argv[1]) is False):
+    print("Takes one argument, the path to and including pathway file")
+    sys.exit()
+
+pwy_file = sys.argv[1]
+
+with open("../../../../Data/PlantSEED_v3/PlantSEED_Roles.json") as subsystem_file:
+    roles_list = json.load(subsystem_file)
+
+roles_dict=dict()
+for entry in roles_list:
+    roles_dict[entry['role']]=entry
+
+with open(pwy_file) as pwy_file_handle:
+    for role in pwy_file_handle.readlines():
+        role=role.strip('\r\n')
+
+        if(role not in roles_dict):
+            print("Warning: role ("+role+") not found in database")
+            continue
+
+        roles_dict[role]['include'] = True
+
+with open('../../../../Data/PlantSEED_v3/PlantSEED_Roles.json','w') as new_subsystem_file:
+    json.dump(roles_list,new_subsystem_file,indent=4)
