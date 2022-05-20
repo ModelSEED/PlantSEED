a528a61894fc010053be33eb45331356fe9020ec
Tristan J. Contant <tjcontant@icloud.com>
2022-05-20 10:18:18 -0500

    Created Add_or_Update_Pathway.py and updated biosynth info
    Created a new version of Add_Pathway that is consistent with Leah's single line approach.
    
    Allows for multiple reactions, features, and and localization specification of each feature.
    
    gls_biosynth updated to support this new system
    
    fixed typo in notes file

--- Scripts/PlantSEED_v3/Curation/tjcontant/Add_or_Update_Pathway.py
+++ Scripts/PlantSEED_v3/Curation/tjcontant/Add_or_Update_Pathway.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os,sys,json
+import os,sys,json,collections
 
 if(len(sys.argv)<2 or os.path.isfile(sys.argv[1]) is False):
     print("Takes one argument, the path to and including pathway file")
@@ -10,11 +10,12 @@ pwy_file = sys.argv[1]
 with open("../../../../Data/PlantSEED_v3/PlantSEED_Roles.json") as subsystem_file:
     roles_list = json.load(subsystem_file)
 
-roles_dict=dict()
+roles_dict = dict()
 rxns_dict  = dict()
 ftrs_dict  = dict()
 
-spontaneous_reactions=list()
+# Adding roles to roles_dict and spontaneous_reactions
+spontaneous_reactions = list()
 for entry in roles_list:
     roles_dict[entry['role']]=entry
     if(entry['role'] == 'Spontaneous Reaction'):
@@ -24,12 +25,13 @@ for entry in roles_list:
     rxns_dict[entry['role']] = entry['reactions']
     ftrs_dict[entry['role']] = entry['features']
 
+# Reading each line in biosynthesis file
 with open(pwy_file) as pwy_file_handle:
     for line in pwy_file_handle.readlines():
         line=line.strip('\r\n')
-        #print(line.split('\t'))
         (rxn,role,ftr,pub,ss,cls,pwy,loc)=line.split('\t')
 
+        # Check if role is new
         new_role = False
         if(role not in roles_dict):
             new_role = True
@@ -37,46 +39,62 @@ with open(pwy_file) as pwy_file_handle:
             if(rxn not in spontaneous_reactions):
                 new_role = True
 
+        ####################################
+        # UPDATE EXISTING ROLE
         if(new_role is False):
-            print("Duplicate Role\t" + role)
+            print("Update Role\t" + role)
 
-            # convert rxn or ftr to list if string
+            # Convert rxn or ftr to list if string
             if(isinstance(rxns_dict[role], str)):
                 rxns_dict[role] = rxns_dict[role].split()
             if(isinstance(ftrs_dict[role], str)):
                 ftrs_dict[role] = ftrs_dict[role].split()
 
-            # find index of role
+            # Split rxn and ftr if possible
+            # Otherwise turn string into list
+            if(';' in rxn): rxn = rxn.split(';')
+            else: rxn = rxn.split()
+            if(';' in ftr): ftr = ftr.split(';')
+            else: ftr = ftr.split()
+
+            # Find index of role
             for i in range(len(roles_list)):
                 if(roles_list[i]['role'] == role): index = i
 
-            # same role
-            if(rxn in rxns_dict[role] and ftr in ftrs_dict[role]):
-                print('\t\t  no new information')
-            # new reaction, same role
-            elif(rxn not in rxns_dict[role]):
-                roles_list[i]['reactions'].append(rxn)
-                print("\t\t  new reaction:\t" + rxn)
-            # new feature, same role
-            elif(ftr not in ftrs_dict[role]):
-                roles_list[i]['features'].append(ftr)
-                print("\t\t  new feature:\t" + ftr)
+            ####################################
+            # New reaction
+            if(not set(rxn).issubset(set(rxns_dict[role]))):
+                for entry in rxn:
+                    if entry not in rxns_dict[role]:
+                        roles_list[index]['reactions'].append(entry)
+                        print("\t\t  new reaction:\t" + entry)
 
-                # Update Localization
-                for entry in loc.split('||'):
-                    (cpt,sources)=entry.split(':')
-                    sources = sources.split('|')
+            ####################################
+            # New feature
+            elif(not set(ftr).issubset(set(ftrs_dict[role]))):
+                for entry in ftr:
+                    if entry not in ftrs_dict[role]:
+                        roles_list[index]['features'].append(entry)
+                        print("\t\t  new feature:\t" + entry)
 
-                    # comparment in role already
-                    if(cpt in roles_list[i]['localization']):
-                        roles_list[i]['localization'][cpt].update({ftr: sources})
-                        print('\t\t  new local:\t' + loc + '\t' + ftr)
-                    # compartment not in role already
-                    else:
-                        roles_list[i]['localization'].update({cpt: {ftr: sources}})
-                        print('\t\t  new local:\t' + loc + '\t' + ftr)
+            ####################################
+            # New Localization
+            loc_dict = dict()
+            for entry in loc.split(';'):
+                if(':' in entry):
+                    (cpt,gene,sources)=entry.split(':')
+                    loc_dict[gene]=sources.split('|')
+                # Assumed reaction compartment
+                else:
+                    cpt=entry
+                    loc_dict[gene]=[]
 
-        # new_role is True
+                if(cpt not in roles_list[index]['localization']):
+                    roles_list[index]['localization'][cpt] = dict()
+                roles_list[index]['localization'][cpt][gene]=loc_dict[gene]
+
+        ####################################
+        # ADD NEW ROLE
         else:
             print("New Role\t" + role)
             new_role = {'role':'',
@@ -99,7 +117,7 @@ with open(pwy_file) as pwy_file_handle:
                 new_role['subsystems'].append(entry)
                 class_dict[entry]=[]
 
-                # If MetaCyc Pathway defined
+                # Add pathway
                 if(pwy != ""):
                     # One pathway for all classes
                     if (len(pwy.split('||')) == 1):
@@ -114,38 +132,45 @@ with open(pwy_file) as pwy_file_handle:
 
             ####################################
             # Add reaction
-            new_role['reactions'].append(rxn)
+            for entry in rxn.split(';'):
+                new_role['reactions'].append(entry)
 
             ####################################
             # Add genes
             if(ftr != ""):
-                new_role['features'].append(ftr)
+                for entry in ftr.split(';'):
+                    new_role['features'].append(entry)
 
             ####################################
             # Add publications
             if(pub != ""):
-                new_role['publications'].append(pub)
+                for entry in pub.split(','):
+                    new_role['publications'].append(entry)
 
             ####################################
             # Add localization
-            for entry in loc.split('||'):
+            for entry in loc.split(';'):
                 loc_dict=dict()
                 #if protein localization data used
                 if(':' in entry):
-                    (cpt,sources)=entry.split(':')
-                    loc_dict[ftr]=sources.split('|')
+                    (cpt,gene,sources)=entry.split(':')
+                    loc_dict[gene]=sources.split('|')
                 #assumed reaction compartment
                 else:
                     cpt=entry
                     loc_dict[rxn]=[]
 
-                new_role['localization'][cpt]=loc_dict
+                if(cpt not in new_role['localization']):
+                    new_role['localization'][cpt] = dict()
+                new_role['localization'][cpt][gene]=loc_dict[gene]
 
+            ####################################
+            # Update dictionaries and roles list
             roles_dict[role] = line
             rxns_dict[role] = rxn
             ftrs_dict[role] = ftr
             roles_list.append(new_role)
 
+# Update roles file with new roles_list
 with open('../../../../Data/PlantSEED_v3/PlantSEED_Roles.json','w') as new_subsystem_file:
     json.dump(roles_list,new_subsystem_file,indent=4)
-#print(json.dumps(roles_list,indent=4))
