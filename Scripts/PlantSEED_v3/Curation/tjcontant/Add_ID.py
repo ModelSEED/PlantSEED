bf2207e9d01e9532e1bafb32a1ad97c406f34d9d
Tristan J. Contant <tjcontant@icloud.com>
2022-05-27 14:07:01 -0500

    Update Add_ID.py check for duplicates and rename key 'id' to 'kbase_id'

--- Scripts/PlantSEED_v3/Curation/tjcontant/Add_ID.py
+++ Scripts/PlantSEED_v3/Curation/tjcontant/Add_ID.py
@@ -7,21 +7,25 @@ with open("../../../../Data/PlantSEED_v3/PlantSEED_Roles.json") as subsystem_fil
 ID_list = list()
 
 for entry in roles_list:
-    # string with unique info for each role
-    # first reaction and subsystem included to differentiate spontaneous rxns
-    str = entry['role'] + entry['reactions'][0] + entry['subsystems'][0]
 
-    # create unique (truncated) hash ID from str and store in list
-    entry['id'] = 'PS_role_' + hashlib.sha256(str.encode('utf-8')).hexdigest()[:6]
-    ID_list.append(entry['id'])
+    # check if id does not exist
+    if('kbase_id' not in entry.keys()):
+        # string with unique info for each role
+        # first reaction and subsystem included to differentiate spontaneous rxns
+        str = entry['role'] + entry['reactions'][0] + entry['subsystems'][0]
 
-# check for collisions (duplicate ID's)
-duplicates = [item for item, count in collections.Counter(ID_list).items() if count > 1]
-if(len(duplicates) > 0):
-    print("Duplicate ID's: " + duplicates)
-else:
-    print("No duplicates!")
+        # create unique (truncated) hash ID from str and store in list
+        entry_id = 'PS_role_' + hashlib.sha256(str.encode('utf-8')).hexdigest()[:6]
 
+        # create new id if already in list
+        while entry_id in ID_list:
+            entry_id = 'PS_role_' + hashlib.sha256(str.encode('utf-8')).hexdigest()[:6]
+
+        entry['kbase_id'] = entry_id
+        print('New ID:\t' + entry_id + '\t' + entry['role'])
+
+    # add id to list
+    ID_list.append(entry['kbase_id'])
 
 with open('../../../../Data/PlantSEED_v3/PlantSEED_Roles.json','w') as new_subsystem_file:
     json.dump(roles_list,new_subsystem_file,indent=4)
