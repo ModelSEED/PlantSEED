5319ab2fee8f5d2c94a5dc1ca53e8cb45b2b6bb8
Hlina Tessema <tesse044@umn.edu>
2025-01-29 09:50:37 -0600

    Completed enzyme reaction printer v1

--- Scripts/PlantSEED_v3/Curation/pelle283/Glucosinolates/BioChemReactionPrinter.py
+++ Scripts/PlantSEED_v3/Curation/pelle283/Glucosinolates/BioChemReactionPrinter.py
@@ -1,72 +1,47 @@
 
-#1/24/2025 Task Hlina Plan of Action
-#open PlantSEED/Data and check all folders
-#create a list of all E.C number values, search for each E.C and print out the reaction that is associated with that
-#
-#if not found, print E.C value __ not found
+#Program takes in string input, searches PlantSEED_Roles.json which is located in PlantSEED/Data/PlantSEED_v3
+# for example Aliphatic desulfoglucosinolate sulfotransferase (EC 2.8.2.38) 
+#print out the list of reactions associated with the role
 
 import os
+import json
 from pathlib import Path
 
-dataBase = "/home/tesse044/Bio_Research/PlantSEED/Data"
-print(dataBase)
-#directory to be opened and searched for enzyme with matching EC number
-
-ec_values = ec_values = {
-    "Step1": ["1.14.14.42", "1.14.14.40", "1.14.14.156", "1.14.13.237"],
-    "Step2": ["1.14.14.43", "1.14.14.45"],
-    "Step4": ["3.4.19.16"],
-    "Step5": ["4.4.1.13", "2.4.1.195"],
-    "Step6": ["EC 2.4.1.195"],
-    "Step7": ["2.8.2.38", "2.8.2.24"]
-}
-
-#This is a dictionary of step keys with the ec nums as values of each key
-#Additional comments about the ec values:
-#EC 1.14.14.45 should be Phenolic and Indolic
+dataBase = "/home/tesse044/Bio_Research/PlantSEED/Data/PlantSEED_v3/PlantSEED_Roles.json"
+#path to PlantSEED_Roles.json file
 
 
-def data_files(dB, all_files):
-    for entry in os.listdir(dB):
-        full_path = os.path.join(dB, entry)
-        #print(full_path)
-        if os.path.isdir(full_path):
-            data_files(full_path, all_files)
-        else:
-            all_files.append(full_path)
-           # data_files(full_path, all_files)
-    return all_files
-
-#data_files returns Data directory as a list of files
-
-
-def find_reaction(ec_num, fileList):
-#function that will find the reaction associated with an ec number and then return it
-    for files in fileList:
-        f = open(files, "r")
-        content = f.read()
-        if (ec_num in content) and ("reactions" in content) and ("role" in content):
-        #if ec_num is found, search for its reaction and return it
-            start_index = content.find("role")
-            end_index = content.find("pathways")
-            reaction_info = content[start_index:end_index]
-            return reaction_info
-            #sub string with role of enzyme info and reaction info is returned
-                
-    return "ec reaction not found"
+def find_reaction(ec_num, plant_json):
+#function that will find the reactions associated with an enzyme and then return it
+    reaction_info = []
+    found = False
+    with open(plant_json, 'r') as openfile:
+        content = json.load(openfile)
+        for info in content: 
+            for item in info:
+                if item == ("role"):
+                    if ec_num in info[item]:
+                        print(ec_num)
+                        found = True
+                if item == ("reactions") and found == True :
+                    start_index = item.find("reactions")
+                    size = len(item)
+                    #while (curr != "localization"):
+                    for i in range (size-1):
+                        reaction_info.append(info[item][start_index])
+                        start_index += 1
+                    found = False
+                    
+    if reaction_info:
+        return reaction_info
+            #list of enzyme reactions if present in plantseed json file
+    else:
+        return "ec reaction not found"
     #if code gets to this point, it means that ec reaction was not found in dataBase             
 
-data = []
-data_files(dataBase, data)
-for stepKey, ec_nums in ec_values.items():
-    print(stepKey + " result: ")
-    for value in ec_nums:
-        print(find_reaction(value,data ) + "\n")
-
-#for loop goes through ec values and prints results determined by find_reaction function
-
-
-
+if __name__ == "__main__": 
+    search_target = input("Enter the name or ec number of enzyme: ")
+    print(find_reaction(search_target, dataBase))
 
 
 
