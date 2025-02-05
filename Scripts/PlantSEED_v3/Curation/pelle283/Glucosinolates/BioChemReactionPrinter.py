091e61b37c0a20df35ca549ac00fc6c61bfd0864
Hlina Tessema <tesse044@umn.edu>
2025-02-05 08:58:34 -0600

    Fixed bug, works for all EC on google doc

--- Scripts/PlantSEED_v3/Curation/pelle283/Glucosinolates/BioChemReactionPrinter.py
+++ Scripts/PlantSEED_v3/Curation/pelle283/Glucosinolates/BioChemReactionPrinter.py
@@ -21,27 +21,39 @@ def find_reaction(ec_num, plant_json):
             for item in info:
                 if item == ("role"):
                     if ec_num in info[item]:
-                        print(ec_num)
+                       # print(ec_num + " this ec is in the info!")
                         found = True
                 if item == ("reactions") and found == True :
                     start_index = item.find("reactions")
-                    size = len(item)
-                    #while (curr != "localization"):
-                    for i in range (size-1):
+                    for i in range (len(info[item])):
                         reaction_info.append(info[item][start_index])
                         start_index += 1
                     found = False
                     
-    if reaction_info:
+    if (len(reaction_info)) > 0:
+        print("Results for: " + ec_num)
         return reaction_info
             #list of enzyme reactions if present in plantseed json file
     else:
+       # print (count)
         return "ec reaction not found"
     #if code gets to this point, it means that ec reaction was not found in dataBase             
 
 if __name__ == "__main__": 
-    search_target = input("Enter the name or ec number of enzyme: ")
-    print(find_reaction(search_target, dataBase))
+    # search_target = input("Enter the name or ec number of enzyme: ")
+    # print(find_reaction(search_target, dataBase))
+
+
+    print("Testing with all enzymes:\n")
+    ec_List = ["EC 1.14.14.42", "EC 1.14.14.40", "EC 1.14.14.156", "EC 1.14.14.43","EC 1.14.14.45",
+     "EC 1.14.14.45","EC 3.4.19.16", "EC 4.4.1.13", "EC 2.4.1.195"]
+    for i in range (len(ec_List)):
+        target = ec_List[i]
+        print(find_reaction(target, dataBase))
+    #result: EC 2.8.2.24 and EC 1.14.13.237 not found
+    #true negative for EC 1.14.13.237, it is not in plantSEED_Roles.json
+    #EC 2.8.2.24 is in plantSEED_Roles.json but no reactions listed for it
+
 
 
 
